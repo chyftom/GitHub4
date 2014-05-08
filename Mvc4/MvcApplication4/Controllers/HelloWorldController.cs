73e652dc2c700665aeaeda0579493e3539542dca
chyftom <214807096@qq.com>
2014-05-08 18:14:45 +0800

    mvc 4 hello world

---
+++ Mvc4/MvcApplication4/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MvcApplication4.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Welcome(string name, int numTimes = 1)
+		{
+			ViewBag.Message = "Hello " + name;
+			ViewBag.NumTimes = numTimes;
+
+			return View();
+		}
+    }
+}
